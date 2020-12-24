ef09a8f62dd3f05efbd794c981d148040ee878d9
Spaghetum <73882272+Spaghetum@users.noreply.github.com>
2020-12-24 10:58:28 +0100

    Create hat.lua

---
+++ hat.lua
@@ -0,0 +1,59 @@
+   	local args = {
+		[1] = "Hat",
+		[2] = 4690305028
+	}
+
+	game:GetService("ReplicatedStorage").ClothingService:FireServer(unpack(args))
+	
+	local args = {
+		[1] = "Hat",
+		[2] = 4627469896
+	}
+
+	game:GetService("ReplicatedStorage").ClothingService:FireServer(unpack(args))
+	
+	local args = {
+		[1] = "Hat",
+		[2] = 4623675984
+	}
+
+	game:GetService("ReplicatedStorage").ClothingService:FireServer(unpack(args))
+	
+	local args = {
+		[1] = "Hat",
+		[2] = 4602619613
+	}
+
+	game:GetService("ReplicatedStorage").ClothingService:FireServer(unpack(args))
+	
+	local args = {
+		[1] = "Hat",
+		[2] = 4315489767
+	}
+
+	game:GetService("ReplicatedStorage").ClothingService:FireServer(unpack(args))
+	
+	local args = {
+		[1] = "Hat",
+		[2] = 4794315940
+	}
+
+	game:GetService("ReplicatedStorage").ClothingService:FireServer(unpack(args)
+	
+	
+	
+	local args = {
+		[1] = "Hat",
+		[2] = 4461505493
+	}
+
+	game:GetService("ReplicatedStorage").ClothingService:FireServer(unpack(args)
+	
+	local args = {
+		[1] = "Hat",
+		[2] = 4461584953
+	}
+
+	game:GetService("ReplicatedStorage").ClothingService:FireServer(unpack(args)
+
+	
